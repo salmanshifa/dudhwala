9403a84fb0f17415da5a6c42a059b424a7fe56b5
Mohd Shahniyaz <BAM20143@superioruniform.com>
2021-01-14 16:19:10 +0530

    This is my first commit!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import $ from 'jquery';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App row content">
+     <Header/>
+     <Content/>
+     <Footer/>      
     </div>
   );
+}
+
+class Header extends React.Component {
+  render() {
+     return (
+      <div class="col-sm-3 sidenav">
+      <h4>John's Blog</h4>
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Home</a></li>
+        <li><a href="#section2">Friends</a></li>
+        <li><a href="#section3">Family</a></li>
+        <li><a href="#section3">Photos</a></li>
+      </ul><br/>
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search Blog.."/>
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
+      </div>
+    </div>
+     );
+  }
+}
+class Content extends React.Component {
+  render() {
+     return (
+      <div class="col-sm-9">
+        <h4><small>RECENT POSTS</small></h4>
+        <hr/>
+        <h2>I Love Food</h2>
+      </div>
+     );
+  }
+}
+
+class Footer extends React.Component{
+  render() {
+    return (
+      <footer class="container-fluid">
+      <p>Footer Text</p>
+    </footer>
+    );
+ }
 }
 
 export default App;
